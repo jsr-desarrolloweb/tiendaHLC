0766a73e30ab57aed77df27226d352ab6c5f6009
jsr <jsr.desarrolloweb@gmail.com>
2020-12-06 12:29:03 +0100

    SistemaGestionEmpleados lee fichero

--- src/init/Init.java
+++ src/init/Init.java
@@ -4,13 +4,17 @@ package init;
 //Clase que se encarga de ejecutar la aplicación principal.
 
 import empleados.Empleado;
+import sistemas.SistemaGestionEmpleados;
+
+import java.io.IOException;
 
 public class Init {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//
+//        Empleado e = new Empleado(1, "jaime", "jaime", 1100 );
+//        System.out.println(e);
 
-        Empleado e = new Empleado(1, "jaime", "jaime", 1100 );
-        System.out.println(e);
-
+        SistemaGestionEmpleados sge = new SistemaGestionEmpleados();
 
     }
 }
--- src/sistemas/SistemaGestionEmpleados.java
+++ src/sistemas/SistemaGestionEmpleados.java
@@ -1,11 +1,38 @@
 package sistemas;
 
 import empleados.Empleado;
-import java.util.ArrayList;
+
+import java.io.*;
+import java.util.HashMap;
 
 public class SistemaGestionEmpleados {
 
-    private ArrayList<Empleado> empleadosEmpresa;
+    private HashMap<Integer, Empleado> empleadosEmpresa;
+
+    public SistemaGestionEmpleados() throws IOException {
+        //Al instanciar un objeto de la clase SistemaGestionEmpleados se cargan los datos del fichero "datosEmpleados.txt"
+
+        //          try/catch
+
+        try{
+
+            File f = new File("src/sistemas/datosEmpleados.txt");
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+            while ((linea = br.readLine()) !=null) {
+                System.out.println(linea);
+            }
+
+
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+
+
+    }
+
 
 
 }
